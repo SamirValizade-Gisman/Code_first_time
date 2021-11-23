1f9b98da5429815ddfe629cf3261a6d271f8aa67
SamirValizade-Gisman <93484165+SamirValizade-Gisman@users.noreply.github.com>
2021-11-23 15:51:11 +0400

    Funksiya və döngülərdən istifadə
    #Gisman

---
+++ Code_16_def_for_20211123.py
@@ -0,0 +1,12 @@
+def factor(number):
+    fact=1
+    for i in range(1,number+1):
+        fact *=i
+    print("Faktoriyal:",fact)
+
+reqem=int(input("Reqemi daxil edin: "))
+
+factor(reqem)
+
+#Funksiya və döngülərdən istifadə
+#Gisman
